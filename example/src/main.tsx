b9a0e0eaf0d1b348dac7199510f2aff832b20563
rajdip-b <agentR47@gmail.com>
2022-11-10 18:01:33 +0530

    feat: Update example

---
+++ example/src/main.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App1 from './App1'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <App1 />
+)
